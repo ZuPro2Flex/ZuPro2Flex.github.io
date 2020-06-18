f4262a36b3de38bb42f399aa480d79e2875eba01
Adrian Singer <adrian.singer@iwu.fraunhofer.de>
2020-06-18 10:17:25 +0200

    jekyll template cleanup and preparation for new project

---
+++ assets/js/init.js
@@ -0,0 +1,74 @@
+(function ($) {
+
+  var intervall = null;
+
+  $(document).ready(function () {
+    initMaterialize();
+    $("#play-btn").on("click", playCarousel);
+    $("#pause-btn").on("click", pauseCarousel);
+
+    // autoplay of the banner
+    setTimeout(playCarousel, 7000);
+  });
+
+  // functions 
+
+  function initMaterialize() {
+
+    M.AutoInit();
+
+    $('.scrollspy').scrollSpy( {
+      scrollOffset: 0
+    });
+
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.slider').slider();
+    $('.sidenav').sidenav();
+    $('.modal').modal();
+  
+    $('.pushpin').pushpin({
+      top: 150,
+      offset: 75
+    });
+
+    $('#home-carousel').carousel({
+      fullWidth: true,
+      indicators: true,
+      duration: 300,
+      onCycleTo: function(e) {
+        manualCarousel();
+      }
+    });
+
+    $('.carousel').carousel({
+      indicators: true,
+      fullWidth: true,
+    });
+  }
+
+
+  function triggerCarousel() {
+    $("#home-carousel").carousel('next');
+  }
+
+  function pauseCarousel() {
+    clearInterval(intervall);
+    intervall = null;
+  }
+
+  function playCarousel() {
+    if (intervall === null) {
+      triggerCarousel();
+      intervall = setInterval(triggerCarousel, duration);
+    }
+  }
+
+  function manualCarousel() {
+    if (intervall !== null) {
+      pauseCarousel();
+      intervall = setInterval(triggerCarousel, duration);
+    }
+  }
+
+})(jQuery); // end of jQuery name space
